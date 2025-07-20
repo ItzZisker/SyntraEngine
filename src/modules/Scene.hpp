0e839dae59da0b98fe51f2967e50dfde05b2f5d8
riske <riskerdegree911@gmail.com>
2025-07-20 14:23:38 +0330

    cleanup

--- src/modules/Scene.hpp
+++ src/modules/Scene.hpp
@@ -30,12 +30,33 @@ struct DirLight {
     glm::vec3 specular = {0.5f, 0.5f, 0.5f};
 };
 
+struct PointLight {
+    glm::vec3 position = {0.0f, 1.0f, 0.0f};
+    glm::vec3 ambient = {0.05f, 0.05f, 0.05f};
+    glm::vec3 diffuse = {0.8f, 0.8f, 0.8f};
+    glm::vec3 specular = {1.0f, 1.0f, 1.0f};
+    float constant = 1.0f;
+    float linear = 0.09f;
+    float quadratic = 0.032f;
+};
+
+struct SpotLight {
+    glm::vec3 position = {0.0f, 1.0f, 0.0f};
+    glm::vec3 direction = {-0.5f, -1.0f, -0.5f};
+    glm::vec3 ambient = {0.05f, 0.05f, 0.05f};
+    glm::vec3 diffuse = {0.8f, 0.8f, 0.8f};
+    glm::vec3 specular = {1.0f, 1.0f, 1.0f};
+    float constant = 1.0f;
+    float linear = 0.09f;
+    float quadratic = 0.032f;
+    float cutOff = glm::cos(glm::radians(12.5f));
+    float outerCutOff = glm::cos(glm::radians(15.0f));
+};
+
 class Scene : public SDL_EventHandler, public DuplexRenderable
 {
 private:
     ShadowMapper* shadowMapper = nullptr;
-
-    SDL_Window* callbackSDLWindow = nullptr;
 
     Shader screenShader = Shader("shaders/screenVertex.glsl", "shaders/screenFrag.glsl");
     Shader batchShader = Shader("shaders/batchVertex.glsl", "shaders/batchFrag.glsl");
