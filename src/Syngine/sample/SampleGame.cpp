2ca375f2dffbf20c353f8e50a9f3925a570b27c5
riske <riskerdegree911@gmail.com>
2025-10-08 20:25:18 +0330

    CMake cleanup for win32 builds

--- src/Syngine/sample/SampleGame.cpp
+++ src/Syngine/sample/SampleGame.cpp
@@ -126,7 +126,7 @@ void SampleGame::createWindow(GameWindow *window) {
 
     DirLight dayLight = {
         {-0.86f, -1.0f, -0.97f},
-        {0.7f, 0.7f, 0.4f},
+        {0.3f, 0.3f, 0.2f},
         {0.5f, 0.5f, 0.3f},
         {0.6f, 0.6f, 0.45f}
     };
